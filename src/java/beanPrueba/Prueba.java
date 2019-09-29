397f582d638af85e3d80017d5b2dd86b9e46e094
Jhowalk <jhoan.harry@gmail.com>
2019-09-29 11:38:20 -0500

    Login + bean de prueba y ejemplo
    se agrego un bean de prueba el cual el login le pasa los datos y muestra la pagina prueba.xhtml con con los datos ingresados

---
+++ src/java/beanPrueba/Prueba.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beanPrueba;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean
+@RequestScoped
+public class Prueba {
+
+    String usuario="";
+    String password="";
+    
+    public Prueba() {
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String funciona(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+     return "prueba1.xhtml";
+    }
+}
