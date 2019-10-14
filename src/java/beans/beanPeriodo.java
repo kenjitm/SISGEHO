9e6a51891a4eaca1644a55837bdfb31dae7ef593
JhonR <johnrestrepo.sistemas@gmail.com>
2019-10-14 12:11:33 -0500

    Periodos

--- src/java/beans/beanPeriodo.java
+++ src/java/beans/beanPeriodo.java
@@ -5,13 +5,46 @@
  */
 package beans;
 
+import entity.Horario;
+import entity.Periodos;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.RowEditEvent;
+
 /**
  *
  * @author SougiroHylian
  */
+@ManagedBean
+@RequestScoped
 public class beanPeriodo {
+    private Periodos Periodos;
     private int id;
-    private String descripcion;
+    private String periodo;
+    private List<Periodos> filteredPeriodos;  
+    public Periodos getPeriodos() {
+        return Periodos;
+    }
+
+    public void setPeriodos(Periodos Periodos) {
+        this.Periodos = Periodos;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
 
     public int getId() {
         return id;
@@ -21,12 +54,40 @@ public class beanPeriodo {
         this.id = id;
     }
 
-    public String getDescripcion() {
-        return descripcion;
-    }
+    
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+  
+    public beanPeriodo() {
+        Periodos = new Periodos();
+    }
+    public String irPeriodos() {
+        return "periodos.xhtml";
+    }
+    public List<Periodos> obtenerPeriodos() {
+        EntityManagerFactory emf
+                = Persistence.createEntityManagerFactory("SisgehoPU");
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Periodos> q = em.createNamedQuery("Periodos.findAll", Periodos.class);
+        return q.getResultList();
+    }
+    public List<Periodos> getFilteredPeriodos() {  
+        return filteredPeriodos;  
+    }  
+    public void guardarPeriodos() {
+        Periodos periodo = new Periodos();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SisgehoPU");
+        EntityManager em = emf.createEntityManager();
+        periodo.setId(id);
+        periodo.setPeriodo(this.periodo);
+        
+        em.getTransaction().begin();
+        em.persist(periodo);
+        em.getTransaction().commit();
+        
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "ÈXITO", "Registro realizado satisfactoriamente");
+        // For PrimeFaces < 6.2
+        RequestContext.getCurrentInstance().showMessageInDialog(message);
+
     }
     
 }
