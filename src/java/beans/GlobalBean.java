e0821723f56dc5640366b1331e58b8da6e237cc4
Kenji Tanaka <kenjitm@outlook.com>
2019-10-30 23:11:52 -0500

    Variable de session global en SISGEHO

---
+++ src/java/beans/GlobalBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import entity.Asignatura;
+import entity.TipoSemestre;
+import entity.Usuario;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author Kenji
+ */
+@ManagedBean
+@RequestScoped
+public class GlobalBean {
+
+    public GlobalBean() {
+        this.getLoggedUser();
+    }
+
+    public String getLoggedUser() {
+        String usuarioLogueado = (String) getObjectFromSession("user");
+        return usuarioLogueado;
+    }
+
+    public String getUserRoles() {
+        String rolesCadena = (String) getObjectFromSession("roles");
+        return rolesCadena;
+    }
+
+    private Object getObjectFromSession(String key) {
+        return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(key);
+    }
+
+    private void saveObjectInSession(String value, String key) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getSessionMap().put(value, key);
+    }
+
+}
